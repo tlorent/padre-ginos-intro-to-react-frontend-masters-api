81e338e2ecfdac50c8a803634d8659f73815e286
Tim Lorent <tim.lorent@icloud.com>
2026-02-11 11:00:10 +0100

    refactor: prepare live deploy

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
 import fastify from "fastify";
-import fastifyStatic from "@fastify/static";
-import path from "path";
-import { fileURLToPath } from "url";
 import { AsyncDatabase } from "promised-sqlite3";
 
 const server = fastify({
@@ -14,15 +11,9 @@ const server = fastify({
 
 const PORT = process.env.PORT || 3000;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const HOST = ("RENDER" in process.env) ? `0.0.0.0` : `localhost`;
 
 const db = await AsyncDatabase.open("./pizza.sqlite");
-
-server.register(fastifyStatic, {
-  root: path.join(__dirname, "public"),
-  prefix: "/public/",
-});
 
 server.get("/api/pizzas", async function getPizzas(req, res) {
   const pizzasPromise = db.all(
@@ -53,7 +44,7 @@ server.get("/api/pizzas", async function getPizzas(req, res) {
       name: pizza.name,
       category: pizza.category,
       description: pizza.description,
-      image: `/public/pizzas/${pizza.pizza_type_id}.webp`,
+      image: `/pizzas/${pizza.pizza_type_id}.webp`,
       sizes,
     };
   });
@@ -93,7 +84,7 @@ server.get("/api/pizza-of-the-day", async function getPizzaOfTheDay(req, res) {
     name: pizza.name,
     category: pizza.category,
     description: pizza.description,
-    image: `/public/pizzas/${pizza.id}.webp`,
+    image: `/pizzas/${pizza.id}.webp`,
     sizes: sizeObj,
   };
 
@@ -138,7 +129,7 @@ server.get("/api/order", async function getOrders(req, res) {
 
   const orderItems = orderItemsRes.map((item) =>
     Object.assign({}, item, {
-      image: `/public/pizzas/${item.pizzaTypeId}.webp`,
+      image: `/pizzas/${item.pizzaTypeId}.webp`,
       quantity: +item.quantity,
       price: +item.price,
     }),
@@ -259,7 +250,7 @@ server.get("/api/past-order/:order_id", async function getPastOrder(req, res) {
 
     const formattedOrderItems = orderItems.map((item) =>
       Object.assign({}, item, {
-        image: `/public/pizzas/${item.pizzaTypeId}.webp`,
+        image: `/pizzas/${item.pizzaTypeId}.webp`,
         quantity: +item.quantity,
         price: +item.price,
       }),
@@ -299,7 +290,7 @@ server.post("/api/contact", async function contactForm(req, res) {
 
 const start = async () => {
   try {
-    await server.listen({ port: PORT });
+    await server.listen({ host: HOST, port: PORT });
     console.log(`Server listening on port ${PORT}`);
   } catch (err) {
     console.error(err);
@@ -308,3 +299,14 @@ const start = async () => {
 };
 
 start();
+
+server.addHook('preHandler', (req, res, done) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST");
+    res.header("Access-Control-Allow-Headers",  "*");
+    const isPreflight = /options/i.test(req.method);
+    if (isPreflight) {
+        return res.send();
+    }  
+    done();
+})
